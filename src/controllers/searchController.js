c6fefec759434bb0c95fc1ace06482a1c3b949ec
vaidik chouhan <vaidikchauhan333@gmail.com>
2025-02-14 16:17:14 +0530

    updated code

---
+++ src/controllers/searchController.js
@@ -0,0 +1,43 @@
+const axios = require("axios");
+const aws4 = require("aws4");
+
+
+
+
+const search = async (req, res) => {
+    
+    const { query } = req.query;
+    if (!query) {
+      return res.status(400).json({ error: "❌ Query parameter is required!" });
+    }
+    const accessKey = process.env.ALGOLIA_API_KEY;
+    const secretKey = process.env.ALGOLIA_SECRET_KEY;
+    const region = process.env.ALGOLIA_REGION;
+    const service = process.env.ALGOLIA_SERVICE_NAME;
+    const baseUrl = process.env.ALGOLIA_BASE_URL;
+    try {
+      const options = {
+        host: new URL(baseUrl).host,
+        path: `/dev/search?query=${encodeURIComponent(query)}`,
+        method: "GET",
+        service,
+        region,
+      };
+  
+      // Sign the request
+      aws4.sign(options, {
+        accessKeyId: accessKey,
+        secretAccessKey: secretKey,
+      });
+  
+      // Make request to AWS
+      const response = await axios.get(`${baseUrl}?query=${query}`, {
+        headers: options.headers,
+    });
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+module.exports = { search };
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const authRoutes = require("./routes/auth.js");
 const tanyaShoppingAssistant = require("./routes/tanyaShoppingAssistant.js");
-
+const searchRoutes = require("./routes/search.js");
 dotenv.config();
 
 const app = express();
@@ -14,9 +14,9 @@ app.use(cors({ origin: "*" }));
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Server is running" });
 });
-
 app.use("/api", authRoutes);
 app.use("/api", tanyaShoppingAssistant);
+app.use("/api", searchRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
