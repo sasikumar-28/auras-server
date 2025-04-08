905372286eceb57511ac13a813e1c793b2d2b9b1
Manoj Duraisamy <manoj.duraisamy@aspiresys.kl>
2025-04-08 11:53:15 +0530

    Account api addded

--- src/routes/account.js
+++ src/routes/account.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const { login, getCustomerInfoById } = require("../controllers/accountController");
+const { login, getCustomerInfoById , customerAddAddress, signUp} = require("../controllers/accountController");
 
 router.post("/customers/login", async (req, res) => {
     try {
@@ -15,6 +15,24 @@ router.post("/customers/login", async (req, res) => {
 router.get("/customers/:customerId", async (req, res) => {
     try {
         const result = await getCustomerInfoById(req); 
+        res.json(result);
+    } catch (error) {
+        res.status(500).json({ message: error, error: error });
+    }
+});
+
+router.post("/address/:customerId/:emailId", async (req, res) => {
+    try {
+        const result = await customerAddAddress(req); 
+        res.json(result);
+    } catch (error) {
+        res.status(500).json({ message: error, error: error });
+    }
+});
+
+router.post("/customers/signin", async (req, res) => {
+    try {
+        const result = await signUp(req); 
         res.json(result);
     } catch (error) {
         res.status(500).json({ message: error, error: error });
